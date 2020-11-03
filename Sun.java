43c72f600f0752df50346f0611179e4d29801a0f
harristariq45 <harristariq57@gmail.com>
2020-11-03 22:10:32 +0000

    sun class initialisation
    tried to make the sun class work

---
+++ Sun.java
@@ -0,0 +1,14 @@
+public class Sun extends Window {
+    
+    public void drawSolarObject(){
+
+        // drawSolarObject(0,0,60,"YELLOW");
+        // drawSolarObject(double distance, double angle, double diameter, String col)
+        // distance = 0;
+
+
+
+
+
+    }
+}
