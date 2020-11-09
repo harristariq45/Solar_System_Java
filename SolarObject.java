943b9142720990c7496cc879ae518e4f0dcbe744
harristariq45 <harristariq57@gmail.com>
2020-11-09 06:01:44 +0000

    solarobject class created replaced planets class
    class created makes the program easier to change if someone else was to work onit.

---
+++ SolarObject.java
@@ -0,0 +1,169 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+public class SolarObject 
+{
+    public int distance;
+    public double angle;
+    public int diameter;
+    public String col;
+    // public Color col;
+
+    public SolarObject(int x, double y, int diameter, String col)
+    {
+        this.distance = x;
+        this.angle = y;
+        this.diameter = diameter;
+        this.col = col;
+    }
+
+    public void Planet(SolarSystem a)
+    {
+        a.drawSolarObject(this.diameter, this.angle, diameter, col);
+        
+    }
+    public double getAngle()
+    {
+        return angle;
+    }
+
+    public String getCol()
+    {
+        return col;
+    }
+
+    public void move(double speed)
+    {
+        angle=angle+speed;
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public double getDiameter()
+    {
+        return diameter;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class SolarObject extends Window
+// {
+//     public int x;
+//     public int y;
+//     public int diameter;
+//     // public String color;
+//     // color = col; 
+//     public Colour col;
+
+//     public SolarObject(int x, int y, int diameter, Colour col)
+//     {
+//         this.x = x;
+//         this.y = y;
+//         this.diameter = diameter;
+//         this.col= col;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class Planets
+// {  
+//     public double distance;
+//     public double angle;
+//     public double diameter;
+//     public String col;
+
+//     public void SolarPlanet(double distanceM, double angleM, double diameterM, String colM)
+//     {
+//         distance = distanceM;
+//         angle = angleM;
+//         diameter = diameterM;
+//         col = colM;
+
+//         drawSolarObject(distance, angle, diameter, col);
+//     }
+
+  
+// }
+
+    // public static Window window;
+
+    // public void Sun(double distance, double angle, double diameter, String col)
+    // {
+    //     // drawSolarObject(0,0,60,"YELLOW");
+    //     distance = 0;
+    //     angle = 0;
+    //     diameter = 60;
+    //     col = "YELLOW";
+    // }
+
+// public class Sun extends Window
+// {
+//     /**
+// 	 *
+// 	 */
+// 	private static final long serialVersionUID = 1L;
+
+//     drawSolarObject(0,0,60,"YELLOW");
+
+// }
--- Window.java
+++ Window.java
@@ -13,10 +13,14 @@ public class Window extends SolarSystem
 {
 
     private static SolarSystem SolarSystem;
+    int movement=0;
+    int x = movement;
+    SolarObject earth = new SolarObject(150,0,15,"BLUE");
+    SolarObject[] asteroids = new SolarObject[200];
 
 /**
  * creates a window of the solar system
- * creates instances of the object planets using 
+ * creates instances sof the object planets using 
  * drawSolarObject 
  * objects can be displayed in the solar system
  * @param movement is the speed of movement of the planet
@@ -25,14 +29,23 @@ public class Window extends SolarSystem
     {
 
         super(1000,1000);
-        int movement=0;
+
+       // super = a;
+
+        // int movement=0;
+        // SolarObject earth = new SolarObject(150,x,15,"BLUE");
 
         while(true)
         {
             drawSolarObject(0,0,60,"YELLOW");
-            int x = movement;
-            movement = movement+1;
-            Solarplanet Venus = new Solarplanet(90,2,15,"CYAN");
+            drawSolarObject(earth.getDistance(),earth.getAngle(), earth.getDiameter(), earth.getCol());
+        
+            earth.move(1);
+            // earth = drawSolarObject(150,x,15,"BLUE");
+            
+            // e.Planet(super);
+
+            // Solarplanet Venus = new Solarplanet(90,2,15,"CYAN");
             // drawSolarObject(150,x,15,"BLUE");
             // drawSolarObjectAbout(150,x,10,"GREY",20,x*2);
             // drawSolarObject(90,x,15,"CYAN");
@@ -48,7 +61,6 @@ public static void main( String[] args){
     new Window();
 
 }
-
 
 }
 
